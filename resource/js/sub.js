79bc3505d98a7035454d097da60354917154539c
DESKTOP-81NU9PC\박성재 <qkrtjdwo1986@gmail.com>
2022-11-14 23:26:03 +0900

    [orange] medical, meal

--- resource/js/sub.js
+++ resource/js/sub.js
@@ -1,3 +1,18 @@
 $(function () {
-    $('table td').chidren('.btn-wrap').parent('td').addClass('td-form');
+
+});
+
+var $checkMedical = $('.check_all');
+$checkMedical.change(function () {
+    var $this = $(this);
+    var checked = $this.prop('checked'); // checked 문자열 참조(true, false)
+    $('input[name="check_child"]').prop('checked', checked);
+});
+var boxes = $('input[name="check_child"]');
+boxes.change(function () {
+    var boxLength = boxes.length;
+    // 체크된 체크박스 갯수를 확인하기 위해 :checked 필터를 사용하여 체크박스만 선택한 후 length 프로퍼티를 확인
+    var checkedLength = $('input[name="check_child"]:checked').length;
+    var selectAll = (boxLength == checkedLength);
+    $checkMedical.prop('checked', selectAll);
 });
